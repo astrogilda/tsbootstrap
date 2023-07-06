cb353e18cbbd198191e59bb977e7950d19deed93
Sankalp Gilda <sankalp.gilda@gmail.com>
2023-07-06 18:29:45 -0400

    first commit

---
+++ tests/test_bootstrap.py
@@ -0,0 +1,77 @@
+import pytest
+from hypothesis import given, strategies as st, settings
+import numpy as np
+from collections import Counter
+from itertools import chain
+
+from src.bootstrap import MovingBlockBootstrap, StationaryBootstrap, CircularBootstrap
+
+
+bootstrap_classes = [MovingBlockBootstrap,
+                     StationaryBootstrap, CircularBootstrap]
+
+
+class TestBootstrap:
+    class TestErrorConditions:
+
+        @pytest.mark.parametrize("data, block_length, bootstrap_class", [(1, 1, cls) for cls in bootstrap_classes])
+        def test_invalid_data_type(self, data, block_length, bootstrap_class):
+            with pytest.raises(TypeError):
+                bootstrap_class(data).split(block_length)
+
+        @pytest.mark.parametrize("block_length, bootstrap_class", [(i, cls) for i in [-1, 0] for cls in bootstrap_classes])
+        def test_non_positive_block_length(self, block_length, bootstrap_class):
+            tsb = bootstrap_class([1, 2, 3, 4, 5])
+            with pytest.raises(ValueError):
+                list(tsb.split(block_length))
+
+        @pytest.mark.parametrize("bootstrap_class", bootstrap_classes)
+        def test_block_length_too_large(self, bootstrap_class):
+            tsb = bootstrap_class([1, 2, 3, 4, 5])
+            with pytest.raises(ValueError):
+                list(tsb.split(6))
+
+        @pytest.mark.parametrize("bootstrap_class", bootstrap_classes)
+        def test_empty_data(self, bootstrap_class):
+            tsb = bootstrap_class([])
+            with pytest.raises(ValueError):
+                list(tsb.split(1))
+
+    class TestNormalConditions:
+
+        @given(st.lists(st.floats(allow_nan=False), min_size=1), st.integers(min_value=1))
+        @settings(deadline=None)
+        @pytest.mark.parametrize("bootstrap_class", bootstrap_classes)
+        def test_same_length(self, data, block_length, bootstrap_class):
+            if block_length <= len(data):
+                tsb = bootstrap_class(data)
+                for train_index, _ in tsb.split(block_length):
+                    assert len(train_index) == len(data)
+
+        @given(st.lists(st.floats(allow_nan=True, allow_infinity=True), min_size=2), st.integers(min_value=1))
+        @settings(deadline=None)
+        @pytest.mark.parametrize("bootstrap_class", bootstrap_classes)
+        def test_nan_values_counts(self, data, block_length, bootstrap_class):
+            tsb = bootstrap_class(data)
+
+            original_nan_or_inf = [str(x) if np.isnan(
+                x) or np.isinf(x) else x for x in data]
+            original_counts = Counter(original_nan_or_inf)
+
+            bootstrap_iterations = 100
+            bootstrap_counts = Counter()
+            for _ in range(bootstrap_iterations):
+                for train_index, _ in tsb.split(block_length):
+                    bootstrap_nan_or_inf = [str(x) if np.isnan(
+                        x) or np.isinf(x) else x for x in train_index]
+                    bootstrap_counts.update(bootstrap_nan_or_inf)
+
+            assert all(key in bootstrap_counts for key in original_counts)
+
+        @given(st.lists(st.floats(allow_nan=True, allow_infinity=True), min_size=2), st.integers(min_value=1))
+        @settings(deadline=None)
+        @pytest.mark.parametrize("bootstrap_class", bootstrap_classes)
+        def test_nan_values_len(self, data, block_length, bootstrap_class):
+            tsb = bootstrap_class(data)
+            for train_index, _ in tsb.split(block_length):
+                assert len(train_index) == len(data)
---
+++ utils/block_length_sampler.py
@@ -0,0 +1,138 @@
+from numba.experimental import jitclass
+from numba import int64, optional
+import numpy as np
+import random
+
+spec = [
+    ('block_length_distribution', int64),
+    ('avg_block_length', int64),
+    ('random_seed', optional(int64)),
+]
+
+
+@jitclass(spec)
+class BlockLengthSampler:
+    """
+    A class for sampling block lengths for the random block length bootstrap.
+
+    Attributes
+    ----------
+    block_length_distribution : int
+        The selected block length distribution function, represented as an integer.
+    avg_block_length : int
+        The average block length to be used for sampling.
+    random_seed : int, optional
+        Random seed for reproducibility. If None, the global random state is used.
+    """
+
+    def __init__(self, block_length_distribution: str, avg_block_length: int, random_seed: int = None):
+        """
+        Initialize the BlockLengthSampler with the selected distribution and average block length.
+
+        Parameters
+        ----------
+        block_length_distribution : str
+            The block length distribution function to use, represented by its name as a string.
+        avg_block_length : int
+            The average block length to be used for sampling.
+        random_seed : int, optional
+            Random seed for reproducibility, by default None. If None, the global random state is used.
+        """
+        self.block_length_distribution = self.distribution_name_to_int(
+            block_length_distribution)
+        self.avg_block_length = avg_block_length
+        self.random_seed = random_seed
+
+        if self.random_seed is not None:
+            np.random.seed(self.random_seed)
+            random.seed(self.random_seed)
+
+    def distribution_name_to_int(self, distribution_name: str) -> int:
+        """
+        Convert a block length distribution name to its corresponding integer representation.
+
+        Parameters
+        ----------
+        distribution_name : str
+            The name of the block length distribution function.
+
+        Returns
+        -------
+        int
+            The integer representation of the block length distribution.
+        """
+        if distribution_name == 'none':
+            return 0
+        elif distribution_name == 'poisson':
+            return 1
+        elif distribution_name == 'exponential':
+            return 2
+        elif distribution_name == 'normal':
+            return 3
+        elif distribution_name == 'gamma':
+            return 4
+        elif distribution_name == 'beta':
+            return 5
+        elif distribution_name == 'lognormal':
+            return 6
+        elif distribution_name == 'weibull':
+            return 7
+        elif distribution_name == 'pareto':
+            return 8
+        elif distribution_name == 'geometric':
+            return 9
+        elif distribution_name == 'uniform':
+            return 10
+        else:
+            assert distribution_name is not None, f"Unknown block_length_distribution '{distribution_name}'"
+
+    def weibull(self, alpha: float, beta: float) -> float:
+        return beta * (-np.log(1 - np.random.random()))**(1/alpha)
+
+    def pareto(self, alpha: float):
+        u = np.random.uniform()
+        return 1.0 / (u ** (1.0 / alpha))
+
+    def sample_block_length(self) -> int:
+        """
+        Sample a block length from the selected distribution.
+
+        Returns
+        -------
+        int
+            A sampled block length.
+        """
+        if self.block_length_distribution == 0:
+            return self.avg_block_length
+        elif self.block_length_distribution == 1:
+            return int(np.random.poisson(self.avg_block_length))
+        elif self.block_length_distribution == 2:
+            return int(np.random.exponential(self.avg_block_length))
+        elif self.block_length_distribution == 3:
+            return int(np.random.normal(loc=self.avg_block_length, scale=self.avg_block_length / 3))
+        elif self.block_length_distribution == 4:
+            return int(random.gammavariate(alpha=2.0, beta=self.avg_block_length / 2))
+        elif self.block_length_distribution == 5:
+            return int(random.betavariate(alpha=2, beta=2) * (2 * self.avg_block_length - 1) + 1)
+        elif self.block_length_distribution == 6:
+            return int(np.random.lognormal(mu=np.log(self.avg_block_length / 2), sigma=np.log(2)))
+        elif self.block_length_distribution == 7:
+            return int(self.weibull(alpha=1.5, beta=1.0) * self.avg_block_length)
+        elif self.block_length_distribution == 8:
+            return int((self.pareto(alpha=1) + 1) * self.avg_block_length)
+        elif self.block_length_distribution == 9:
+            return int(np.random.geometric(p=1 / self.avg_block_length))
+        elif self.block_length_distribution == 10:
+            return int(np.random.randint(low=1, high=2 * self.avg_block_length))
+        else:
+            assert self.block_length_distribution is not None, f"Unknown block_length_distribution '{self.block_length_distribution}'"
+
+
+"""
+
+# Example usage
+from utils.block_length_sampler import BlockLengthSampler
+block_length_sampler = BlockLengthSampler(block_length_distribution='normal', avg_block_length=10, random_seed=42)
+print(block_length_sampler.sample_block_length())
+
+"""
