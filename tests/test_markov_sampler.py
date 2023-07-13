57ab7387a43c58e6a4445a27b9fc96f9855d9a6f
Sankalp Gilda <sankalp.gilda@gmail.com>
2023-07-13 13:40:13 -0400

    added more tests

--- tests/test_markov_sampler.py
+++ tests/test_markov_sampler.py
@@ -14,8 +14,8 @@ class TestCalculateTransitionProbs:
             """
             Test calculate_transition_probs with random assignments and random n_components.
             """
-            assignments = np.array(assignments)
-            n_components = max(n_components, np.max(assignments) + 1)
+            # Update the assignments to be within the correct range
+            assignments = np.array(assignments) % n_components
             transition_probabilities = MarkovSampler.calculate_transition_probs(
                 assignments, n_components)
             assert transition_probabilities.shape == (
@@ -53,7 +53,7 @@ class TestCalculateTransitionProbs:
             """
             assignments = np.array([1])
             n_components = 1
-            with pytest.raises(AssertionError):
+            with pytest.raises(ValueError):
                 MarkovSampler.calculate_transition_probs(
                     assignments, n_components)
 
@@ -63,7 +63,7 @@ class TestCalculateTransitionProbs:
             """
             assignments = np.array([0, 1, 2, 3, 4])
             n_components = 0
-            with pytest.raises(AssertionError):
+            with pytest.raises(ValueError):
                 MarkovSampler.calculate_transition_probs(
                     assignments, n_components)
 
@@ -73,7 +73,7 @@ class TestCalculateTransitionProbs:
             """
             assignments = np.array([0, 1, 2, 3, 4])
             n_components = 2.5
-            with pytest.raises(AssertionError):
+            with pytest.raises(ValueError):
                 MarkovSampler.calculate_transition_probs(
                     assignments, n_components)
 
@@ -83,7 +83,7 @@ class TestCalculateTransitionProbs:
             """
             assignments = np.array([0, 1, 2, 3, 4])
             n_components = -2
-            with pytest.raises(AssertionError):
+            with pytest.raises(ValueError):
                 MarkovSampler.calculate_transition_probs(
                     assignments, n_components)
 
@@ -93,7 +93,7 @@ class TestCalculateTransitionProbs:
             """
             assignments = np.array([0, 1, 2, 3, 4, 11])
             n_components = 5
-            with pytest.raises(AssertionError):
+            with pytest.raises(ValueError):
                 MarkovSampler.calculate_transition_probs(
                     assignments, n_components)
 
@@ -101,14 +101,35 @@ class TestCalculateTransitionProbs:
 # Test for fit_hidden_markov_model function
 class TestFitHiddenMarkovModel:
     class TestPassingCases:
-        @settings(deadline=None)
-        @given(st.lists(st.lists(st.floats(min_value=-1000, max_value=1000, allow_infinity=False, allow_nan=False), min_size=2, max_size=2), min_size=6, max_size=10), st.integers(min_value=1, max_value=6))
+
+        test_data = [
+            (np.array([[-100, 50], [200, -150], [300, 400],
+             [500, 600], [700, 800], [900, 1000]]), 2),
+            (np.array([[-50, 10], [20, -30], [40, 50],
+             [-60, 70], [80, -90], [100, 110]]), 3),
+            (np.array([[1, 2], [3, 4], [5, 6], [7, 8], [
+             9, 10], [11, 12], [13, 14], [15, 16]]), 4),
+            (np.array([[10, 20], [-30, 40], [50, -60],
+             [70, 80], [90, 100], [110, -120]]), 1),
+            (np.array([[-1000, 2000], [3000, -4000],
+             [5000, 6000], [7000, 8000], [9000, 10000]]), 2),
+            (np.array([[250, -500], [750, 1000],
+             [1250, 1500], [1750, 2000], [2250, 2500]]), 3),
+            (np.array([[100, 200], [300, 400], [500, 600],
+             [700, 800], [900, 1000], [1100, 1200]]), 4),
+            (np.array([[-200, 400], [-600, 800],
+             [1000, -1200], [1400, 1600], [1800, 2000]]), 5),
+            (np.array([[2, 4], [6, 8], [10, 12],
+             [14, 16], [18, 20], [22, 24]]), 1),
+            (np.array([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5],
+             [6, 6], [7, 7], [8, 8], [9, 9], [10, 10]]), 3),
+        ]
+
+        @pytest.mark.parametrize("X, n_states", test_data)
         def test_random_data_random_states(self, X, n_states):
             """
             Test fit_hidden_markov_model with random data and random n_states.
             """
-            # Convert X to a 2D numpy array
-            X = np.array(X)
             model = MarkovSampler.fit_hidden_markov_model(
                 X, n_states, n_fits=1)
             assert isinstance(model, hmm.GaussianHMM)
@@ -118,7 +139,7 @@ class TestFitHiddenMarkovModel:
             """
             Test fit_hidden_markov_model with a single 2D point and n_states=1.
             """
-            X = np.array([[1, 2]])
+            X = np.array([[1, 2], [3, 4], [5, 6]])
             n_states = 1
             model = MarkovSampler.fit_hidden_markov_model(X, n_states)
             assert isinstance(model, hmm.GaussianHMM)
@@ -151,3 +172,88 @@ class TestFitHiddenMarkovModel:
             n_states = 0
             with pytest.raises(ValueError):
                 MarkovSampler.fit_hidden_markov_model(X, n_states)
+
+
+class TestGetBlockElement:
+    class TestPassingCases:
+        def test_first_element(self):
+            """
+            Test get_block_element with block_index="first".
+            """
+            block = np.array([[1, 2], [3, 4], [5, 6]])
+            block_index = "first"
+            result = MarkovSampler.get_block_element(block, block_index)
+            assert np.array_equal(result, np.array([1, 2]))
+
+        def test_middle_element(self):
+            """
+            Test get_block_element with block_index="middle".
+            """
+            block = np.array([[1, 2], [3, 4], [5, 6]])
+            block_index = "middle"
+            result = MarkovSampler.get_block_element(block, block_index)
+            assert np.array_equal(result, np.array([3, 4]))
+
+        def test_last_element(self):
+            """
+            Test get_block_element with block_index="last".
+            """
+            block = np.array([[1, 2], [3, 4], [5, 6]])
+            block_index = "last"
+            result = MarkovSampler.get_block_element(block, block_index)
+            assert np.array_equal(result, np.array([5, 6]))
+
+        def test_single_row_first_element(self):
+            """
+            Test get_block_element with block_index="first" and single-row block.
+            """
+            block = np.array([[1, 2]])
+            block_index = "first"
+            result = MarkovSampler.get_block_element(block, block_index)
+            assert np.array_equal(result, np.array([1, 2]))
+
+        def test_single_row_middle_element(self):
+            """
+            Test get_block_element with block_index="middle" and single-row block.
+            """
+            block = np.array([[1, 2]])
+            block_index = "middle"
+            result = MarkovSampler.get_block_element(block, block_index)
+            assert np.array_equal(result, np.array([1, 2]))
+
+        def test_single_row_last_element(self):
+            """
+            Test get_block_element with block_index="last" and single-row block.
+            """
+            block = np.array([[1, 2]])
+            block_index = "last"
+            result = MarkovSampler.get_block_element(block, block_index)
+            assert np.array_equal(result, np.array([1, 2]))
+
+    class TestFailingCases:
+        def test_invalid_block_index(self):
+            """
+            Test get_block_element with an invalid block_index.
+            """
+            block = np.array([[1, 2], [3, 4], [5, 6]])
+            block_index = "invalid"
+            with pytest.raises(ValueError):
+                MarkovSampler.get_block_element(block, block_index)
+
+        def test_empty_block(self):
+            """
+            Test get_block_element with an empty block.
+            """
+            block = np.array([])
+            block_index = "middle"
+            with pytest.raises(ValueError):
+                MarkovSampler.get_block_element(block, block_index)
+
+        def test_1d_block(self):
+            """
+            Test get_block_element with a 1D block.
+            """
+            block = np.array([1, 2, 3, 4, 5, 6])
+            block_index = "middle"
+            with pytest.raises(ValueError):
+                MarkovSampler.get_block_element(block, block_index)
