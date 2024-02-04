8868a21ade600f5d5653aa72c48d41c38f3db1d5
Franz Király <f.kiraly@ucl.ac.uk>
2024-02-04 22:46:03 +0100

    Update types.py

--- src/tsbootstrap/utils/types.py
+++ src/tsbootstrap/utils/types.py
@@ -1,11 +1,17 @@
 from numbers import Integral
-from typing import Literal
+import sys
+from typing import Any, Literal
 
 from numpy.random import Generator
+from packaging.specifiers import SpecifierSet
 
 ModelTypesWithoutArch = Literal["ar", "arima", "sarima", "var"]
 
 ModelTypes = Literal["ar", "arima", "sarima", "var", "arch"]
+
+
+sys_version = sys.version.split(" ")[0]
+new_typing_available = sys_version in SpecifierSet(">=3.10")
 
 
 def FittedModelTypes():
@@ -23,25 +29,31 @@ def FittedModelTypes():
     )
     return fmt
 
-OrderTypesWithoutNone = (
-    Integral
-    | list[Integral]
-    | tuple[Integral, Integral, Integral]
-    | tuple[Integral, Integral, Integral, Integral]
-)
+if new_typing_available:
+    OrderTypesWithoutNone = (
+        Integral
+        | list[Integral]
+        | tuple[Integral, Integral, Integral]
+        | tuple[Integral, Integral, Integral, Integral]
+    )
 
-OrderTypes = None | OrderTypesWithoutNone
+    OrderTypes = None | OrderTypesWithoutNone
 
-RngTypes = None | Generator | Integral
+    RngTypes = None | Generator | Integral
 
-BlockCompressorTypes = Literal[
-    "first",
-    "middle",
-    "last",
-    "mean",
-    "mode",
-    "median",
-    "kmeans",
-    "kmedians",
-    "kmedoids",
-]
+    BlockCompressorTypes = Literal[
+        "first",
+        "middle",
+        "last",
+        "mean",
+        "mode",
+        "median",
+        "kmeans",
+        "kmedians",
+        "kmedoids",
+    ]
+else:
+    OrderTypesWithoutNone = Any
+    OrderTypes = Any
+    RngTypes = Any
+    BlockCompressorTypes = Any
