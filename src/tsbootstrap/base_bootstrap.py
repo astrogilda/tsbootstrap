e2ff589bf2d58a682ba534a6f59b41e4ec98c148
Franz Király <f.kiraly@ucl.ac.uk>
2024-02-24 00:43:29 +0100

    Update base_bootstrap.py

--- src/tsbootstrap/base_bootstrap.py
+++ src/tsbootstrap/base_bootstrap.py
@@ -240,7 +240,7 @@ class BaseResidualBootstrap(BaseTimeSeriesBootstrap):
     """
 
     _tags = {
-        "python_dependencies": ["arch", "statsmodels"],
+        "python_dependencies": "statsmodels",
         "bootstrap_type": "residual",
         "capability:multivariate": False,
     }
